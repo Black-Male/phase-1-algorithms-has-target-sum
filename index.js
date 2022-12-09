316ae500b27ef36f92f9cbfb4b837ab762e5b4d1
Black-Male <Gkaranja20@gmail.com>
2022-12-09 16:53:27 +0300

    Work Done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
-/* 
-  Write the Big O time complexity of your function here
+/*
+make a function that checks if 2 nums from the array adds up to 
+a target
 */
 
+function hasTargetSum(array, target) {
+  // Write your algorithm here
+ for (let i = 0; i< array.length; i++){
+  const duo = target - array [i]
+    for (let j = i + 1; j< array.length; j++){
+      if (array[j] === duo) return true
+    }
+  }
+  return false
+}
+
+
+
 /* 
+  Write the Big O time complexity of your function here'
+  runtime: O(n^2)
+*/
+
+/*  if (array === [3, 8, 12, 4, 11, 7]){
+    target = 10
+    function 
+
+  }
   Add your pseudocode here
+  check if it is a given array,
+    run a function if it is the given array()
+      checks the numbers one by one to see if any returns the chosen sum(target)
+        returns true if sum is there
+      
+
 */
 
 /*
   Add written explanation of your solution here
+  my solution is a function which loops through the 
+  array and works through it backwards subtracting the pulled 
+  array value from the target and checking if another "Different"
+  array element will be equals to the subtracted value
 */
 
 // You can run `node index.js` to view these console logs
